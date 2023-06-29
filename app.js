5d39c0a41db40cc36df0e73d291647ee270eee96
seaweed78 <adhikarikshitiz12@gmail.com>
2023-06-29 14:46:12 +0545

    add bootstrap

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const path = require("path");
 
 const PORT = 3000;
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+);
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
